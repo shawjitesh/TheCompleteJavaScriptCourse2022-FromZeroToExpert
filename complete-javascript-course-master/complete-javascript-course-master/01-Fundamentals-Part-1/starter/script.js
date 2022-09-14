4d117b36f3647f38de06dab68408ac6e2e7f64a5
shawjitesh <jiteshshaw93@gmail.com>
2022-09-14 20:47:49 +0530

    #3 master: Values and Variables

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,24 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Jitesh");
+console.log(23);
+
+let firstName = "Asmita";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let asmita_jitesh = "AJ";
+let $function = 27;
+
+let person = "jitesh";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Husband";
+
+let job1 = "programmer";
+let job2 = "husband";
+
+console.log(myFirstJob);
