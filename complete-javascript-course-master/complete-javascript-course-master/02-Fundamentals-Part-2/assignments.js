f6518a0290357e7ae27d1bc641b89bf85eabfca4
shawjitesh <jiteshshaw93@gmail.com>
2022-09-18 08:29:15 +0530

    #34 master: Functions

---
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,142 @@
+/*
+console.log("====================");
+console.log("Assignment for Values and Variables");
+
+const country = "India";
+const continent = "Asia"
+let population = 20000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log("====================");
+console.log("Assignment for Data Types");
+
+let isIsland = false;
+const language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log("====================");
+console.log("Assignment for let, const and var");
+
+language = "Hindi";
+// country = "Indonesia";
+
+console.log("====================");
+console.log("Assignment for Basic operators");
+
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+console.log(population++);
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+
+let avgPopulation = 33000000;
+console.log(population < avgPopulation);
+
+const description = country + " is in " + continent + ", and it's " + population / 1000000 + " million people speak " + language;
+console.log(description);
+
+console.log("====================");
+
+console.log("Assignment for Strings and Template Literals");
+
+const country = "India";
+const continent = "Asia";
+let population = 20000000;
+const language = "Hindi";
+
+const description = `${country} is in ${continent}, and it's ${population / 1000000} millon people speak ${language}`;
+console.log(description);
+
+console.log("====================");
+console.log("Assignment for Taking Decisions: If - Else Statements");
+
+const country = "India";
+let population = 130000000;
+const avgPopulation = 33000000;
+
+if (population > avgPopulation) {
+    console.log(`${country}'s population is above average`);
+} else {
+    const diffInPopulation = (avgPopulation - population) / 1000000;
+    console.log(`${country}'s population is ${diffInPopulation} million below average`);
+}
+
+console.log("====================");
+console.log("Assignment for Type Conversion and Coercion");
+
+console.log('9' - '5');
+console.log('19' - '13' + '17');
+console.log('19' - '13' + 17);
+console.log('123' < 57);
+console.log(5 + 6 + '4' + 9 - 4 - 2);
+
+console.log("====================");
+console.log("Assignment for Equality Operators: == vs ===");
+
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+
+if (numNeighbours === 1) console.log("Only 1 border!");
+else if (numNeighbours > 1) console.log("More than 1 border");
+else console.log("No borders");
+
+console.log("====================");
+console.log("Assignment for Logical Operators");
+
+const country = prompt("What is your country's name?");
+const language = prompt("Which language do you speak?");
+const population = Number(prompt(`What is the population of ${country}`));
+let isIsland = prompt(`Is ${country} an island?`);
+if (isIsland === "yes") isIsland = true;
+else isIsland = false;
+
+if (language === "English" && population < 50000000 && !isIsland) {
+    console.log(`You should live in ${country} 😊`);
+} else {
+    console.log(`${country} does not meet your criteria 😞`);
+}
+
+console.log("====================");
+console.log("Assignment for The Switch Statement");
+
+const language = prompt("Which language do you speak?");
+
+switch (language) {
+    case "chinese":
+    case "mandarin":
+        console.log("Most number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("Number 4");
+        break;
+    case "arabic":
+        console.log("5th most spoken language");
+        break;
+    default:
+        console.log("Great language too 😃");
+}
+
+console.log("====================");
+console.log("Assignment for The Conditional(Ternary) Operator");
+
+const country = prompt("What is your country's name?");
+let population = Number(prompt(`What is the population of ${country}?`));
+const avgPopulation = 33000000;
+
+population > avgPopulation ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
+
+console.log("====================");
+*/
---
+++ complete-javascript-course-master/complete-javascript-course-master/02-Fundamentals-Part-2/codingChallenges.js
@@ -0,0 +1,89 @@
+/*
+console.log("====================");
+console.log("Coding Challenge #1");
+
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+const markHigherBMI = markBMI > johnBMI;
+console.log(markBMI, johnBMI, markHigherBMI);
+
+console.log("====================");
+console.log("Coding Challenge #2");
+
+// const markMass = 78;
+// const markHeight = 1.69;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const markHeight = 1.88;
+
+const johnMass = 85;
+const johnHeight = 1.76;
+
+const markBMI = markMass / markHeight ** 2;
+const johnBMI = johnMass / johnHeight ** 2;
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`);
+} else {
+    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})!`);
+}
+
+console.log("====================");
+console.log("Coding Challenge #3");
+
+let dolphinsScore = Number(prompt("How much did Dolphins score in match #1?"));
+let koalasScore = Number(prompt("How much did Koalas score in match #1?"));
+
+let totalDolphinsScore = dolphinsScore;
+let totalKoalasScore = koalasScore;
+
+dolphinsScore = Number(prompt("How much did Dolphins score in match #2?"));
+koalasScore = Number(prompt("How much did Koalas score in match #2?"));
+
+totalDolphinsScore += dolphinsScore;
+totalKoalasScore += koalasScore;
+
+dolphinsScore = Number(prompt("How much did Dolphins score in match #3?"));
+koalasScore = Number(prompt("How much did Koalas score in match #3?"));
+
+totalDolphinsScore += dolphinsScore;
+totalKoalasScore += koalasScore;
+
+const avgDolphinsScore = totalDolphinsScore / 3;
+const avgKoalasScore = totalKoalasScore / 3;
+
+if (avgDolphinsScore > avgKoalasScore && avgDolphinsScore >= 100) {
+    console.log("Dolphins win the trophy! 🐬");
+} else if (avgDolphinsScore < avgKoalasScore && avgKoalasScore >= 100) {
+    console.log("Koalas win the trophy! 🐨");
+} else if (avgDolphinsScore == avgKoalasScore && avgDolphinsScore >= 100) {
+    console.log("Both Dolphins & Koalas share the Winning Trophy! 🐬🏆🐨");
+} else {
+    console.log("No team wins!!! 😶");
+}
+
+console.log("====================");
+console.log("Coding Challenge #4");
+
+const billAmount = Number(prompt("What is the bill amount Jitesh?"));
+
+billAmount >= 50 && billAmount <= 300 ? console.log(`The bill was ${billAmount}, the tip was ${billAmount * 0.15}, and the total value ${billAmount + (billAmount * 0.15)}`) : console.log(`The bill was ${billAmount}, the tip was ${billAmount * 0.2}, and the total value ${billAmount + (billAmount * 0.2)}`);
+
+console.log("====================");
+*/
