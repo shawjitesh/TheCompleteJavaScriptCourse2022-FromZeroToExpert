0bf371619e24ab43e9bc7b0659a2ae42333d86ba
shawjitesh <jiteshshaw93@gmail.com>
2022-09-20 23:13:27 +0530

    #76 master: Project #1 - Manipulating CSS Styles

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -19,11 +19,20 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No secretNumber!";
-  } else if (guess === secretNumber) {
+  }
+  // When player wins
+  else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
-  } else if (guess > secretNumber) {
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+  }
+  // When guess is too high
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
       score--;
@@ -32,7 +41,9 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "💥 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  // When guess is too low
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
       score--;
