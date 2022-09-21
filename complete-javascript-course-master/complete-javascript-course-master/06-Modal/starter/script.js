733beb87c0dea910cc2013a6aca4709550f1d8a4
shawjitesh <jiteshshaw93@gmail.com>
2022-09-21 20:31:00 +0530

    #83 master: Project #3 - Pig Game

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -22,7 +22,6 @@ btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
 document.addEventListener("keydown", function (event) {
-  console.log(event.key);
   if (event.key === "Escape" && !modal.classList.contains("hidden"))
     closeModal();
 });
