8ff2694a3ae82dd067194ef86cc8a62df00de688
shawjitesh <jiteshshaw93@gmail.com>
2022-09-21 16:32:12 +0530

    #81 master: Project #2 - Working with Classes

--- complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/06-Modal/starter/script.js
@@ -4,7 +4,20 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsShowModal = document.querySelectorAll(".show-modal");
-console.log(btnsShowModal);
+
+const openModal = function () {
+  console.log("Button clicked");
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 for (let i = 0; i < btnsShowModal.length; i++)
-  console.log(btnsShowModal[i].textContent);
+  btnsShowModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
